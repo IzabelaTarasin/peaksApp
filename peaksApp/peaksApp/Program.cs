f0d470beff180d8881dad509b574dab346d2f6f9
Izabela <izabela.tarasin@gmail.com>
2022-11-16 17:01:43 +0100

    base project

---
+++ peaksApp/peaksApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace peaksApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

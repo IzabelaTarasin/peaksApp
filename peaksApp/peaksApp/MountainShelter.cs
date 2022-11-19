4896cc5da57ed899df690496fe16fc0c5d8f5038
Izabela <izabela.tarasin@gmail.com>
2022-11-19 15:27:14 +0100

    added new classes

---
+++ peaksApp/peaksApp/MountainShelter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace peaksApp
+{
+    public class MountainShelter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ peaksApp/peaksApp/Suppliers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace peaksApp
+{
+    public class Suppliers
+    {
+        public int Id { get; set; }
+        public int MountainShelterId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
